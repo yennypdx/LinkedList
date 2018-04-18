c4d1dc59aa499aa14b441cbbc1375b6b6535d455
ypurwantopdx <yenny.purwanto@oit.edu>
2018-04-17 23:24:25 -0700

    * work in progress

--- DataStructures_CS/DataStructures.Test/LinkedListTests.cs
+++ DataStructures_CS/DataStructures.Test/LinkedListTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GenericLinkedList.Test
 {
@@ -6,8 +8,43 @@ namespace GenericLinkedList.Test
     public class LinkedListTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestRemovingFirstElementOnTheList()
         {
+            LinkedList<int> testList = new LinkedList<int>();
+            testList.AddLast(4);
+            testList.AddLast(3);
+            testList.AddLast(2);
+            testList.AddLast(1);
+            
+            testList.Remove(4);
+            Assert.IsTrue(testList.Count == 3);
+            Assert.IsTrue(testList.First.Value == 3);
         }
+
+        [TestMethod]
+        public void TestRemovingLastElementOnTheList()
+        {
+            LinkedList<int> testList = new LinkedList<int>();
+            testList.AddLast(4);
+            testList.AddLast(3);
+            testList.AddLast(2);
+            testList.AddLast(1);
+
+            testList.Remove(1);
+            Assert.IsTrue(testList.Count == 3);
+            Assert.IsTrue(testList.Last.Value == 2);
+        }
+
+        //[TestMethod]
+        //public void TestRemovingElementInBetweenElementsOnTheList()
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        //[TestMethod]
+        //public void TestRemovingElementNotInTheList()
+        //{
+        //    throw new System.NotImplementedException();
+        //}
     }
 }
--- DataStructures_CS/DataStructures/LinkedList.cs
+++ DataStructures_CS/DataStructures/LinkedList.cs
@@ -1,11 +1,17 @@
-﻿namespace DataStructures
+﻿using System;
+
+namespace DataStructures
 {
     public class LinkedList<T> : ILinkedList<T>
     {
         public int Count { get; set; }
         public ILinkedListNode<T> First { get; set; }
-        public bool IsEmpty { get; }
+        public bool IsEmpty
+        {
+            get { return Last == null; }
+        }
         public ILinkedListNode<T> Last { get; set; }
+
         public void AddFirst(T value)
         {
             throw new System.NotImplementedException();
@@ -28,13 +34,50 @@
 
         public ILinkedListNode<T> Find(T value)
         {
-            throw new System.NotImplementedException();
+           if (!this.Contains(value)) { return null; }
+                
+            ILinkedListNode<T> node = this.First;
+            while (!node.Value.Equals(value)){ node = node.Next; }
+                
+            return node;
         }
 
-        public bool Remove(T value)
+        public bool Remove(T value) 
         {
-            throw new System.NotImplementedException();
-        }
+            if (this.Contains(value)) {
+                ILinkedListNode<T> currentNode = Find(value);
 
+                //Node is the only item
+                if (First == Last) {
+                    First = null;
+                    Last = null;
+                }
+                else {
+                    //Node is the first node
+                    if (currentNode.Previous == null) {
+                        First = First.Next;
+                        First.Previous = null;
+                        First.Next = null;
+                    }
+                    //Node is the last node
+                    if (currentNode.Next == null) {
+                        Last = Last.Previous;
+                        Last.Previous = null;
+                        Last.Next = null;
+                    }
+                    //Node in the middle of two nodes
+                    if (currentNode.Next == null && currentNode.Previous == null) {
+                        currentNode.Previous.Next = currentNode.Next;
+                        currentNode.Next.Previous = currentNode.Previous;
+                        currentNode = null;
+                    }
+                }
+                Count--;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
 }
